9439250457f6726ae1e351d3f0d72b359176ec7f
Bhashit Joshi <bhashit@cyntexa.com>
2020-12-16 12:44:07 +0530

    first commit

---
+++ AuthenticationToken.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var client_secret = document.getElementById('cs');
+console.log('Client_secret->',client_secret);
+var client_id = document.getElementById('ci');
+console.log('Client_id-> ',client_id);
+var url = document.getElementById('url');
+console.log('url-> ',url);
+var options = {
+  'method': 'POST',
+  'url': url+'/v2/token',
+  'headers': {
+  },
+  formData: {
+    'grant_type': 'grant_type',
+    'client_id': client_id,
+    'client_secret': client_secret
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
